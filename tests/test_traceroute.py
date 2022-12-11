62d8769ab2bbebdd9daff44251e698dc040b4ef2
Dmitry Volodin <dv@nocproject.org>
2022-12-11 12:17:37 +0100

    Skip tests on permission denied

--- tests/test_traceroute.py
+++ tests/test_traceroute.py
@@ -7,6 +7,7 @@
 
 # Python modules
 import asyncio
+import socket
 
 # Third-party modules
 import pytest
@@ -15,6 +16,18 @@ import pytest
 from gufo.traceroute import Traceroute
 
 
+def _check_permissions():
+    try:
+        socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        return True
+    except PermissionError:
+        return False
+
+
+HAS_PERMISSIONS = _check_permissions()
+
+
+@pytest.mark.skipif(not HAS_PERMISSIONS, reason="Permission denied")
 def test_traceroute_ipv4():
     async def inner():
         hops = []
@@ -31,6 +44,7 @@ def test_traceroute_ipv4():
         assert hop.addr == "127.0.0.1"
 
 
+@pytest.mark.skipif(not HAS_PERMISSIONS, reason="Permission denied")
 def test_traceroute_ipv6():
     async def inner():
         hops = []
@@ -43,6 +57,7 @@ def test_traceroute_ipv6():
         asyncio.run(inner())
 
 
+@pytest.mark.skipif(not HAS_PERMISSIONS, reason="Permission denied")
 def test_tos():
     async def inner():
         async with Traceroute(tos=1) as tr:
